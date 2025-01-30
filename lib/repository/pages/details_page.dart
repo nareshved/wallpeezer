2fb78644915c152a52afa26d6dfed011dff3fac0
Naresh Kumar <nareshved1996@gmail.com>
2025-01-30 20:42:13 +0530

    added set and saved wallpaper

--- lib/repository/pages/details_page.dart
+++ lib/repository/pages/details_page.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:wallpaper/wallpaper.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key, required this.photo});
@@ -8,10 +13,141 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final isPage = MediaQuery.sizeOf(context);
     return Scaffold(
-      body: SizedBox(
-        width: isPage.width,
-          height: isPage.height,
-          child: Image.network(photo, fit: BoxFit.fitWidth,)),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: isPage.width,
+            height: isPage.height,
+            child: CachedNetworkImage(
+              imageUrl: photo, fit: BoxFit.fitWidth,
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+          
+          Positioned(
+            bottom: 60, left: 30, right: 30,
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 60, height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                    borderRadius: BorderRadius.circular(9)
+                  ),
+                  child: Center(child: IconButton(onPressed: (){
+                   applyImg(context, isPage);
+                  }, icon: Icon(Icons.edit, color: Theme.of(context).colorScheme.primary, size: 33,)),),
+                ),Container(
+                  width: 60, height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                    borderRadius: BorderRadius.circular(9)
+                  ),
+                  child: Center(child: IconButton(onPressed: (){
+                    saveImage(context);
+                  }, icon: Icon(Icons.save_alt, color: Theme.of(context).colorScheme.primary, size: 33,)),),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
+
   }
+
+
+ void applyImg(BuildContext context, Size isPage) async{
+
+  showDialog(context: context, builder: (context) {
+    return AlertDialog(
+      title: const Text("Choose Where to Set Wallpaper"),
+      actions: [
+        TextButton(onPressed: (){
+
+         var imgStream = Wallpaper.imageDownloadProgress(photo);
+
+         imgStream.listen((event) {
+           log(event);
+         },
+           onDone: () async {
+         var check = await  Wallpaper.homeScreen(
+             width: isPage.width,
+             height: isPage.height,
+             options: RequestSizeOptions.resizeFit
+           ).then((value) {
+             ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Wallpaper Apply Home Screen")));
+           },);
+
+         log(check);
+
+           },
+           onError: (e) {
+             ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text("$e")));
+           }
+
+         );
+
+
+        }, child: const Text("Home Screen")),
+        TextButton(onPressed: (){
+
+          var imgStream = Wallpaper.imageDownloadProgress(photo);
+
+          imgStream.listen((event) {
+            log(event);
+          },
+              onDone: () async {
+             var check  = await  Wallpaper.lockScreen(
+                    width: isPage.width,
+                    height: isPage.height,
+                    options: RequestSizeOptions.resizeFit
+                ).then((value) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Wallpaper Apply Lock Screen")));
+                },);
+               log(check);
+              },
+              onError: (e) {
+                ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text("$e")));
+              }
+
+          );
+        }, child: const Text("Lock Screen")),
+        TextButton(onPressed: (){
+          var imgStream = Wallpaper.imageDownloadProgress(photo);
+
+          imgStream.listen((event) {
+            log(event);
+          },
+              onDone: ()async  {
+              var check = await  Wallpaper.bothScreen(
+                    width: isPage.width,
+                    height: isPage.height,
+                    options: RequestSizeOptions.resizeFit
+                ).then((value) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Wallpaper Apply Both Screen")));
+                },);
+              log(check);
+              },
+              onError: (e) {
+                ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text("$e")));
+              }
+
+          );
+        }, child: const Text("Both Screen")),
+
+      ],
+    );
+  },);
+ }
+
+ void saveImage(BuildContext context){
+  GallerySaver.saveImage(photo).then((value) {
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Wallpaper Saved Success")));
+  },);
+ }
+
 }
--- lib/repository/pages/homepage.dart
+++ lib/repository/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wallpeezer/data/bloc/user_register/register_bloc.dart';
@@ -21,15 +22,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final TextEditingController searchController = TextEditingController();
-
 
   @override
   void initState() {
     super.initState();
     //
-     BlocProvider.of<WallpaperBloc>(context).add(GetSearchWallpaperEvent(query: "red car"));
+    BlocProvider.of<WallpaperBloc>(context)
+        .add(GetSearchWallpaperEvent(query: "red car"));
     //
     // BlocProvider.of<WallpaperBloc>(context)
     //     .add(GetTrendingWallpaperEvent());
@@ -67,7 +67,6 @@ class _HomePageState extends State<HomePage> {
       ),
       appBar: AppBar(
         title: const Text(AppInfo.appName),
-
       ),
       body: BlocBuilder<WallpaperBloc, WallpaperStates>(
         builder: (context, state) {
@@ -79,7 +78,7 @@ class _HomePageState extends State<HomePage> {
 
           if (state is WallpaperErrorState) {
             log(state.errorMsg);
-           return Center(child: Text(state.errorMsg));
+            return Center(child: Text(state.errorMsg));
           }
 
           if (state is WallpaperLoadedState) {
@@ -87,39 +86,60 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(11.0),
-                  child: CustomSearchBar(searchController: searchController,),
+                  child: CustomSearchBar(
+                    searchController: searchController,
+                  ),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: GridView.builder(
                       shrinkWrap: true,
                       itemCount: state.mData.photos!.length,
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          childAspectRatio: 9/16,
-                            mainAxisSpacing: 12,
-                            crossAxisSpacing: 12,
-                            crossAxisCount: 2),
-                        itemBuilder: (context, index) {
-                          var eachPhoto = state.mData.photos![index].src!.portrait;
-                          return InkWell(
-                            onTap: (){
-                           Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage(photo: eachPhoto),));
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(11),
-                              child: Image.network(eachPhoto!, fit: BoxFit.fill,),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              childAspectRatio: 9 / 16,
+                              mainAxisSpacing: 12,
+                              crossAxisSpacing: 12,
+                              crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        var eachPhoto =
+                            state.mData.photos![index].src!.portrait;
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailsPage(photo: eachPhoto),
+                                ));
+                          },
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(11),
+                            child: CachedNetworkImage(
+                              imageUrl: eachPhoto!,
+                              fit: BoxFit.fill,
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
                             ),
-                          );
-                        },),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ],
             );
           }
 
-          return Container(color: Colors.red, width: 100, height: 100,);
+          return Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          );
         },
       ),
     );
